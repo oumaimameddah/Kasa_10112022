3ae952533b34f627c0e2feb388cdffe2894c002d
Oumaim Meddah <oumaimameddah.98@gmail.com>
2022-11-22 00:58:20 +0100

    Étape 3 : Ajouter un Header et un Footer

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,11 +5,13 @@ export default class KasaRouter extends React.Component {
     render() {
         return (
             <Router forceRefresh={true}>
+                <div>HEADER</div>
                 <Routes>
                     <Route path="/home" element={<div>HOME</div>} exact/>
-                    <Route path="/about" element={<div>HOME</div>} exact/>
+                    <Route path="/about" element={<div>ABOUT</div>} exact/>
                     <Route path="/" element={<div>404</div>}/>
                 </Routes>
+                <div>FOOTER</div>
             </Router>
         )
     }
