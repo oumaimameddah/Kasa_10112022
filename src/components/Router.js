59239c01267b643e1e05d63287e021bf769f0d54
Oumaim Meddah <oumaimameddah.98@gmail.com>
2022-11-30 03:51:21 +0100

    Étape 4 : Creer la page Home, terminer la page

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
+import PropTypes from "prop-types";
 
-export default class KasaRouter extends React.Component {
+export default class KasaRouter extends PureComponent {
     render() {
+        const { housingsData } = this.props
         return (
             <Router forceRefresh={true}>
                 <Header/>
                 <Routes>
-                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/home" element={<Home housingsData={housingsData} />}/>
                     <Route path="/about" element={<div>ABOUT</div>}/>
                     <Route path="/" element={<div>404</div>}/>
                 </Routes>
