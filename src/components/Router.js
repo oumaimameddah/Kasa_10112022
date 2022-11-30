f3d1cc43e12add4929dc35cd22a3c64c7e2f4c2e
Oumaim Meddah <oumaimameddah.98@gmail.com>
2022-11-30 04:02:10 +0100

    Étape 5 : Creer la page A propos

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./Header";
 import Footer from "./Footer";
 import Home from "./Home";
-import PropTypes from "prop-types";
+import About from "./About";
 
 export default class KasaRouter extends PureComponent {
     render() {
@@ -13,7 +13,7 @@ export default class KasaRouter extends PureComponent {
                 <Header/>
                 <Routes>
                     <Route path="/home" element={<Home housingsData={housingsData} />}/>
-                    <Route path="/about" element={<div>ABOUT</div>}/>
+                    <Route path="/about" element={<About/>}/>
                     <Route path="/" element={<div>404</div>}/>
                 </Routes>
                 <Footer/>
