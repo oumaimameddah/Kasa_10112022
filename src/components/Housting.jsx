1ed162693566c5bd4c063ab2ef444b8adde47a4b
Oumaim Meddah <oumaimameddah.98@gmail.com>
2022-12-06 00:23:10 +0100

    Étape 7 : Creer new Compenent

---
+++ src/components/Housting.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { PureComponent } from 'react';
+
+export default class Housing extends PureComponent{
+    render() {
+        return (
+            <div>NEW COMPONENT</div>
+        )
+    }
+}
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 import Home from "./Home";
 import About from "./About";
 import Error from "./Error";
+import Housing from "./Housting";
 
 export default class KasaRouter extends PureComponent {
     render() {
@@ -15,7 +16,7 @@ export default class KasaRouter extends PureComponent {
                 <Routes>
                     <Route path="/home" element={<Home housingsData={housingsData} />}/>
                     <Route path="/about" element={<About/>}/>
-                    <Route path="/housing/:id" exact element={<div>VOUS AVEZ CLIQUER SUR UN ELEMENTS</div>}/>
+                    <Route path="/housing/:id" exact element={<Housing />}/>
                     <Route path='*' exact={true} element={<Error code="404"/>}/>
                 </Routes>
                 <Footer/>
